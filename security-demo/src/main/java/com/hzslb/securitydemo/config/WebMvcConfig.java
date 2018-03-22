fffab13474dff5f785f654f8a95b997a2e384889
githubdruid <itemdruid@gmail.com>
2018-03-22 22:27:17 +0800

    添加注释

--- security-demo/src/main/java/com/hzslb/securitydemo/config/WebMvcConfig.java
+++ security-demo/src/main/java/com/hzslb/securitydemo/config/WebMvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        //注册访问/login转向login.html页面
         registry.addViewController("/login").setViewName("login");
     }
 }
