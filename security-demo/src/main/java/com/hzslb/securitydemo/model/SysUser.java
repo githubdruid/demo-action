6b7577d3d3650b10e645fa73903ffefe37db5ce3
githubdruid <itemdruid@gmail.com>
2018-03-23 18:26:16 +0800

    暂时提交

--- security-demo/src/main/java/com/hzslb/securitydemo/model/SysUser.java
+++ security-demo/src/main/java/com/hzslb/securitydemo/model/SysUser.java
@@ -1,12 +1,20 @@
 package com.hzslb.securitydemo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.validation.constraints.Max;
+import java.util.Collection;
 import java.util.List;
 
-public class SysUser {
+public class SysUser implements UserDetails {
     private Integer id;
     private String username;
+    @JsonIgnore
     private String password;
+    private String rawPassword;
+    @JsonIgnore
     private List<SysRole> roles;
 
     public Integer getId() {
@@ -21,8 +29,33 @@ public class SysUser {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     public String getPassword() {
